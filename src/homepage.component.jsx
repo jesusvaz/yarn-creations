a3aa0209c439c952090b99c1c34646b8cb0fb8bc
jesusvazquez <jesusvaz@gmail.com>
2020-02-27 14:09:48 -0700

    crated project with create react app and add files for home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const HomePage =() => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div classMame='content'>
+                    <h1 className='title'>KIDS</h1>
+                    <spam className='subtitle'>SHOP NOW</spam>
+                </div>
+            </div>
+        </div>
+ 
+    
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div classMame='content'>
+                <h1 className='title'>WOMAN</h1>
+                <spam className='subtitle'>SHOP NOW</spam>
+            </div>
+        </div>
+    </div>
+
+
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div classMame='content'>
+                <h1 className='title'>HATS</h1>
+                <spam className='subtitle'>SHOP NOW</spam>
+            </div>
+        </div>
+    </div>
+
+
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div classMame='content'>
+                <h1 className='title'>SPECIAL OCATIONS</h1>
+                <spam className='subtitle'>SHOP NOW</spam>
+            </div>
+        </div>
+    </div>
+
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div classMame='content'>
+                <h1 className='title'>SHOES</h1>
+                <spam className='subtitle'>SHOP NOW</spam>
+            </div>
+        </div>
+    </div>
+
+</div>
+);
+
+export default HomePage;
